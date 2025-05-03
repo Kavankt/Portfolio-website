85270b8bfab3776aa92487792dcc9db43bec45f9
Kavankt <114717370+Kavankt@users.noreply.github.com>
2025-05-03 22:21:15 +0530

    navbar

--- src/components/navbar/NavbarToggler.jsx
+++ src/components/navbar/NavbarToggler.jsx
@@ -1,17 +1,25 @@
+// src/components/navbar/NavbarToggler.jsx
+import React from "react";
+import { motion } from "framer-motion";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useDispatch } from "react-redux";
+import { IoMdClose } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../../state/menuSlice";
 
 const NavbarToggler = () => {
   const dispatch = useDispatch();
+  const menuOpen = useSelector((state) => state.menu.menuOpen);
 
   return (
-    <button
+    <motion.button
       onClick={() => dispatch(toggleMenu())}
-      className="p-2 bg-[#0f172a] text-white rounded-md text-3xl hover:bg-[#1e293b] transition"
+      className="p-2 bg-[#0f172a] text-white rounded-md shadow-md text-3xl hover:bg-[#1e293b] transition-colors"
+      animate={{ rotate: menuOpen ? 180 : 0 }}
+      initial={false}
+      transition={{ type: "spring", stiffness: 300, damping: 20 }}
     >
-      <GiHamburgerMenu />
-    </button>
+      {menuOpen ? <IoMdClose /> : <GiHamburgerMenu />}
+    </motion.button>
   );
 };
 
